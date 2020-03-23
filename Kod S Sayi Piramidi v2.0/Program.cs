de29857e2545233929ef7d04bbf34be423acfa56
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-23 03:11:44 +0300

    Initial commit

---
+++ Kod S Sayi Piramidi v2.0/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kod_S_Sayi_Piramidi_v2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            char karakter;
+
+        tekrarbaşla:
+
+            Console.Write("Piramit Basamak Değerini Giriniz:");
+            n = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************************");
+
+            Console.Write("Devam Etmek İçin E/e Çıkmak için H/h basınız: ");
+            karakter = Convert.ToChar(Console.ReadLine());
+
+            if (karakter == 'E' || karakter == 'e')
+            {
+                goto tekrarbaşla;
+            }
+
+            else if (karakter == 'H' || karakter == 'h')
+            {
+
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp_Console_37_Sayi_Piramidi_v.2.0
+ 
